37f6c67f51549a6e3a759b00a27abf859212f7c0
zehelll <aviz.h@escolar.ifrn.edu.br>
2026-09-23 14:39:57 -0300

    Deletando atividade-pratica-02 diretorio

--- atividade-pratica-02/src/solicitacoes/dto/aprovar-solicitacao.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsInt, Min } from "class-validator";
-
-export class AprovarSolicitacaoDto {
-  @IsInt()
-  @Min(1)
-  versao: number;
-}
--- atividade-pratica-02/src/usuarios/usuarios.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsuariosService } from './usuarios.service';
-
-@Module({
-  providers: [UsuariosService],
-  exports: [UsuariosService],
-})
-
-export class UsuariosModule {}
